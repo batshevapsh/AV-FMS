98b32f8f4fa907eadbb03c58b0ea07d8e1dd60e3
batshevapsh <batshevascu@gmail.com>
2022-11-20 23:03:44 +0200

    add models an DB

---
+++ vehicle_management/vehicle_api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Car(models.Model):
+     vehicle_number = models.CharField(max_length=20)
+     availability = models.BooleanField(null=True)
+     manufacturer=models.CharField(max_length=20)
+
+class Task(models.Model):
+     car = models.ForeignKey(Car, on_delete=models.CASCADE)
+     owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+
+
+
+   
