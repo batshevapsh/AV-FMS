1f07317fe5e74e6f7c3e856122d6c8496f30f50f
batshevapsh <batshevascu@gmail.com>
2022-11-27 00:51:12 +0200

    delete

--- vehicle_management/vehicle_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from core.models import Car
-from rest_framework import serializers
-
-
-class CarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Car
-        fields = ['vehicle_number','availability','manufacturer']
--- vehicle_management/vehicle_api/urls.py
+++
@@ -1,8 +0,0 @@
-from vehicle_api.views import car
-from django.urls import path
-
-
-urlpatterns = [
-   
-    path('', car.view_all_cars),
-]
